2f42f46cc41da17fdf3770b338a404ca8a7d8454
AhmedHamdyh <ah2261@fayoum.edu.eg>
2023-03-09 22:51:17 +0200

    animation added to splash viwe

--- lib/features/splash/presintation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presintation/views/widgets/splash_view_body.dart
@@ -3,8 +3,36 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class SplashViweBody extends StatelessWidget {
+class SplashViweBody extends StatefulWidget {
   const SplashViweBody({super.key});
+
+  @override
+  State<SplashViweBody> createState() => _SplashViweBodyState();
+}
+
+class _SplashViweBodyState extends State<SplashViweBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slideAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 5));
+    slideAnimation = Tween<Offset>(begin: const Offset(0, 10), end: Offset.zero)
+        .animate(animationController);
+    animationController.forward();
+    slideAnimation.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +40,14 @@ class SplashViweBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(AssitsData.logo),
-        const Text(
-          'Reed Free Books',
-          textAlign: TextAlign.center,
+        SlideTransition(
+            position: slideAnimation, child: Image.asset(AssitsData.logo)),
+        SlideTransition(
+          position: slideAnimation,
+          child: const Text(
+            'Reed Free Books',
+            textAlign: TextAlign.center,
+          ),
         )
       ],
     );
