c4251a3fa7a7550772c6e9d8f2ecb19a65fc999d
AhmedHamdyh <ah2261@fayoum.edu.eg>
2023-05-16 18:33:14 +0300

    ui  and data model done

---
+++ lib/core/eroors/failures.dart
@@ -0,0 +1,3 @@
+abstract class Failuers {}
+
+class ServerFailuers extends Failuers {}
--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly/core/eroors/failures.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchFeaturedBoox();
-  fetchBestSellerBoox();
+  Future<Either<Failuers, List<BookModel>>> fetchBestSellerBoox();
+  Future<Either<Failuers, List<BookModel>>> fetchFeaturedBoox();
 }
