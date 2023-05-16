b07bf9c53a26e6b58d561a05ff3b27c8dfd8424c
AhmedHamdyh <ah2261@fayoum.edu.eg>
2023-05-16 19:35:03 +0300

    fetch news book

---
+++ lib/features/home/data/repos/home_repo_implemintation.dart
@@ -0,0 +1,34 @@
+import 'package:bookly/core/utilty/api_servece.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/core/eroors/failures.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
+  @override
+  Future<Either<Failuers, List<BookModel>>> fetchNewsedBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest &q=computer science');
+
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      // TODO
+      return left(ServerFailuers());
+    }
+  }
+
+  @override
+  Future<Either<Failuers, List<BookModel>>> fetchFeaturedBoox() {
+    // TODO: implement fetchFeaturedBoox
+    throw UnimplementedError();
+  }
+}
