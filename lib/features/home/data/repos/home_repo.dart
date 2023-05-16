e5d43b1ed3035738083c50d65eedf8cc903fb1d7
AhmedHamdyh <ah2261@fayoum.edu.eg>
2023-05-16 13:03:22 +0300

    ui done and home model created

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,4 @@
+abstract class HomeRepo {
+  fetchFeaturedBoox();
+  fetchBestSellerBoox();
+}
---
+++ lib/features/search/presintation/views/widgets/search_view_body.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../../../../home/presintation/views/widget/Home screen wedgets/best_seller_lis_view_item.dart';
+import 'custm_search_text_filed.dart';
+import 'search_result_list_view.dart';
+
+class SearchViewBody extends StatelessWidget {
+  const SearchViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: Column(
+        children: const [
+          CustomSearchTextFiled(),
+          SizedBox(
+            height: 20,
+          ),
+          Expanded(child: SearchResultListView())
+        ],
+      ),
+    );
+  }
+}
